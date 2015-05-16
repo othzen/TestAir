88b26dad408a8f3ea8add434dccdbaaed220d753
othzen <k.othzen@gmail.com>
2015-05-16 11:42:51 +0200

    Initial commit

---
+++ app/src/main/java/com/kristoferothzen/test/MainActivity.java
@@ -0,0 +1,180 @@
+package com.kristoferothzen.test;
+
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Context;
+import android.view.Display;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import jp.co.olympus.camerakit.OLYCamera;
+import jp.co.olympus.camerakit.OLYCameraConnectionListener;
+import jp.co.olympus.camerakit.OLYCameraKitException;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends Activity implements OLYCameraConnectionListener{
+
+    private final String TAG = this.toString();
+    private Executor connectionExecutor = Executors.newFixedThreadPool(1);
+    private BroadcastReceiver connectionReceiver;
+    private OLYCamera camera;
+    {
+        camera = new OLYCamera();
+        camera.setConnectionListener(this);
+    }
+
+
+
+    public void statusKnappKlickad(View V) {
+        //Visa statusen vid knapptryckningen
+        TextView statusTextBox;
+        statusTextBox = (TextView) findViewById(R.id.statusText);
+        while (!camera.isConnected()){
+            WifiManager wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+            WifiInfo info = wifiManager.getConnectionInfo();
+            if (wifiManager.isWifiEnabled() && info != null && info.getNetworkId() != -1) {
+                startConnectingCamera();
+            }
+        }
+
+    }
+
+    private String startConnectingCamera(){
+        connectionExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+
+                try {
+                    camera.connect();
+                } catch (OLYCameraKitException e) {
+                    Log.w(TAG, "Connect to the camera failed: " + e.getMessage());
+                    return;
+                }
+                try {
+                    camera.changeLiveViewSize(toLiveViewSize(preferences.getString("live_view_quality", "QVGA")));
+                } catch (OLYCameraKitException e) {
+                    Log.w(TAG, "To change the live view size is failed: " + e.getMessage());
+                    return;
+                }
+                try {
+                    camera.changeRunMode(OLYCamera.RunMode.Recording);
+                } catch (OLYCameraKitException e) {
+                    Log.w(TAG, "To change the run-mode is failed: " + e.getMessage());
+                    return;
+                }
+
+                // Restores my settings.
+                if (camera.isConnected()) {
+                    Map<String, String> values = new HashMap<String, String>();
+                    for (String name : Arrays.asList(
+                            "TAKEMODE",
+                            "TAKE_DRIVE",
+                            "APERTURE",
+                            "SHUTTER",
+                            "EXPREV",
+                            "WB",
+                            "ISO",
+                            "RECVIEW"
+                    )) {
+                        String value = preferences.getString(name, null);
+                        if (value != null) {
+                            values.put(name, value);
+                        }
+                    }
+                    if (values.size() > 0) {
+                        try {
+                            camera.setCameraPropertyValues(values);
+                        } catch (OLYCameraKitException e) {
+                            Log.w(TAG, "To change the camera properties is failed: " + e.getMessage());
+                        }
+                    }
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        onConnectedToCamera();
+                    }
+                });
+            }
+        });
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onDisconnectedFromCamera() {
+        /*if (!isDestroyed()) {
+            ConnectingFragment fragment = new ConnectingFragment();
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            transaction.replace(R.id.fragment1, fragment);
+            transaction.commit();
+        }*/
+    }
+
+    @Override
+    public void onDisconnectedByError(OLYCamera camera, OLYCameraKitException e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                onDisconnectedFromCamera();
+            }
+        });
+    }
+}
